b31677636e23011414ed613b9fd079548540344b
Goruchie <marcosgualtero43253@gmail.com>
2023-03-07 19:08:17 -0300

    Generate models

--- app/models/reserve.rb
+++ app/models/reserve.rb
@@ -1,4 +1,7 @@
 class Reserve < ApplicationRecord
-  belongs_to :user_id
-  belongs_to :item_id
+  belongs_to :user
+  belongs_to :item
+
+  validates :date, presence: true
+  
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,10 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable, :validatable,
          :recoverable, :rememberable,
          :jwt_authenticatable, jwt_revocation_strategy: self
- 
+         
+  validates :first_name, presence: true, length: { maximum: 50 }, format: { with: /\A[a-zA-Z0-9]+\z/ }
+  validates :last_name, presence: true, length: { maximum: 50 }, format: { with: /\A[a-zA-Z0-9]+\z/ }
+  validates :email, presence: true, length: { maximum: 50 }
+
+  has_many :reserves, dependent: :destroy
 end
