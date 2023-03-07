f887ce1361eba5e542949aa4d72b8d0abf65669b
Goruchie <marcosgualtero43253@gmail.com>
2023-03-07 18:38:42 -0300

    Generate models

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,5 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable, :validatable,
          :recoverable, :rememberable,
          :jwt_authenticatable, jwt_revocation_strategy: self
+ 
 end
