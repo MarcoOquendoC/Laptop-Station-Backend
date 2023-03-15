1d64643e5eacb2a17ba2ec5e15462128c1207783
R. Mauricio Mendoza H <54081966+rbreva@users.noreply.github.com>
2023-03-15 11:27:03 -0500

    Update seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,24 +10,24 @@ Item.create!([
     title: 'ROG Strix SCAR 15',
     item_model: 'G532LW-AZ088T',
     serial_n: '00000_000_0000000',
-    image: 'https://limalocal.com/laptopstation/images/default.png',
-    description: 'Acelere el trabajo y el juego en una máquina diseñada para ofrecer velocidades de fotogramas de tres dígitos. ROG Boost mejora la GPU GeForce® RTX 2080 SUPER™ de vanguardia a 1560 MHz a 150 W en modo Turbo para un juego rápido y fluido. La poderosa décima generación CPU Intel® Core™ combinada con hasta 32 GB de segmentos de RAM DDR4-3200 a través del trabajo intensivo y el juego. Con 2 veloces SSD M.2 NVMe ejecutándose en RAID 0, cargando los tiempos se aceleran en todas sus aplicaciones y juegos. El espacio para un tercer SSD significa que usted puede expandir fácilmente su almacenamiento y llevar su biblioteca de programas completa a cualquier parte.',
+    image: 'https://limalocal.com/laptopstation/images/rog_strix.png',
+    description: 'NVIDIA® GeForce RTX™ 4080 Laptop GPU. ROG Boost: 2330MHz* at 175W (2280MHz Boost Clock+50MHz OC, 150W+25W Dynamic Boost). 12GB GDDR6',
     brand: 'Asus'
   },
   {
-    title: "HP ENVY Laptop",
-    item_model: "17-cr0747nr",
-    serial_n: "00000_000_0000000",
-    image: "https://limalocal.com/laptopstation/images/default.png",
-    description: "Al combinar la durabilidad con la potencia y el rendimiento, la laptop ThinkPad E15 de 4ta generación te permite hacer las cosas en cualquier lugar. Equipado con hasta procesadores Intel® Core™ i7 de 12va generación con hasta 40 GB de memoria, puede procesar las cargas de trabajo más pesadas. También tiene la opción de conectividad WiFi 6E*, para acceder a Internet más rápido dondequiera que estés.",
-    brand: "HP"
+    title: 'HP ENVY Laptop',
+    item_model: '17-cr0747nr',
+    serial_n: '00000_000_0000000',
+    image: 'https://limalocal.com/laptopstation/images/hp_envy.png',
+    description: 'HP Envy Laptop, 17.3" IPS Touchscreen, Intel Core i7-1255U, GeForce MX 550, Backlit Keyboard, Fingerprint Reader, Wi-Fi 6, Audio by Bang & Olufsen, Win 11 (32GB RAM | 1TB PCIe SSD)',
+    brand: 'HP'
   },
   {
-    title: "ThinkPad E15 4ta Gen",
-    item_model: "21E7S1AM00",
-    serial_n: "00000_000_0000000",
-    image: "https://limalocal.com/laptopstation/images/default.png",
-    description: "potencia y el rendimiento, la laptop ThinkPad E15 de 4ta generación te permite hacer las cosas en cualquier lugar. Equipado con hasta procesadores Intel® Core™ i7 de 12va generación con hasta 40 GB de memoria, puede procesar las cargas de trabajo más pesadas. También tiene la opción de conectividad WiFi 6E*, para acceder a Internet más rápido.",
+    title: 'ThinkPad E15 4ta Gen',
+    item_model: '21E7S1AM00',
+    serial_n: '00000_000_0000000',
+    image: 'https://limalocal.com/laptopstation/images/thinkpade15.png',
+    description: 'AMD Ryzen™ 7 5825U Processor (2.00 GHz up to 4.50 GHz). 15.6" FHD (1920 x 1080), IPS, Anti-Glare, Non-Touch, 45%NTSC, 300 nits, 60Hz, LED Backlight, Narrow Bezel',
     brand: "Lenovo"
   }
 ])
