14d2031e783eaf1d91bbe8c959fab80e5afa686b
Goruchie <marcosgualtero43253@gmail.com>
2023-03-17 09:10:56 -0300

    Fix current_user rswag test error

--- spec/requests/api/current_user_spec.rb
+++ spec/requests/api/current_user_spec.rb
@@ -1,25 +1,35 @@
 require 'swagger_helper'
 
-RSpec.describe CurrentUserController, type: :request do
+describe 'Current User API' do
   path '/current_user' do
-    get 'Returns the current user' do
+    get 'Retrieves the current user' do
       tags 'Current User'
+      security [bearer_auth: []]
       produces 'application/json'
-      security [{ Bearer: [] }]
 
-      response '200', 'returns the current user' do
-        schema '$ref' => '#/components/schemas/User'
+      response '200', 'current user retrieved' do
+        schema type: :object,
+               properties: {
+                 id: { type: :integer },
+                 name: { type: :string },
+                 email: { type: :string, format: :email },
+                 created_at: { type: :string, format: :date_time },
+                 updated_at: { type: :string, format: :date_time }
+               },
+               required: %w[id name email created_at updated_at]
+
+        let(:Authorization) { "Bearer #{token}" }
 
         run_test! do
-          expect(response).to have_http_status(:ok)
-          expect(response.content_type).to eq('application/json')
+          expect(response).to match_json_schema('current_user')
         end
       end
 
       response '401', 'unauthorized' do
-        schema '$ref' => '#/components/schemas/Error'
+        let(:Authorization) { nil }
 
         run_test! do
+          expect(response).to match_json_schema('error')
           expect(response).to have_http_status(:unauthorized)
         end
       end
